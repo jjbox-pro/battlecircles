325a559e08c64f8947c4dad35605a907b319f2bc
denizz <workdenizz@gmail.com>
2022-01-09 13:38:58 +0700

    Added game init; Added infint game loop.

--- web/src/js/spa/battci/view/dom_comps/game.js
+++ web/src/js/spa/battci/view/dom_comps/game.js
@@ -8,16 +8,136 @@ class DOMComp_Game extends DOMComp {
 	constructor() {
 		super();
 	}
-	
+
+	init(){
+		super.init();
+
+		this.initLoop();
+
+		this.scene = (new Scene()).init();
+		
+		this.game = new Game(this, this.scene);
+	}
+
 	calcName() {
 		return 'game';
 	}
 
-	initLoop(){
-		this.processInterval = 17;
+	modifyCont($cont){
+		$cont.append(this.scene.getCanvas().addClass('game__canvas'));
 	}
+
+	bindEvents(){
+		this.keys = {};
+
+		document.addEventListener('keydown', (event) => {
+			this.keys[event.code] = true;
+		});
+		document.addEventListener('keyup', (event) => {
+			this.keys[event.code] = false;
+		});
+
+		this.scene.canvasTag.addEventListener('pointermove', (event) => {
+			this.scene.mouse.x = event.offsetX;
+			this.scene.mouse.y = event.offsetY;
+		});
+
+
+		this.scene.canvasTag.addEventListener('pointerdown', (event) => {
+			this.scene.mouse.down = true;
+		});
+		this.scene.canvasTag.addEventListener('pointerup', (event) => {
+			this.scene.mouse.down = false;
+		});
+	}
+
+	afterShow(firstShow){
+		if( !firstShow )
+			return;
+
+		this.game.init();
+
+		this.startLoop();
+	}
+
+	process() {
+		this.game.process();
+	}
+
+	render() {
+		if (this.stopRender)
+			return;
+
+		this.game.render();
+	}
+
+	initLoop() {
+		this.processInterval = 17;
+		this.processIntervalRev = 1 / this.processInterval;
+	}
+
+	startLoop() {
+		this.process();
+
+		this.render();
+
+		this.timeFrame = this.timeOld = timeMgr.getNowLocMS();
+
+		this.frames = 0;
+
+		this.loop();
+	}
+
+	loop = (() => {
+		let timeNow, timeDelta,	processCount, saveTime;
+
+		return function(){
+			timeNow = timeMgr.getNowLocMS();
+			timeDelta = timeNow - this.timeOld;
+			processCount = (timeDelta * this.processIntervalRev) | 0;
+			saveTime = false;
+
+			if (processCount > 5)
+				processCount = 5;
+
+			console.log(processCount);
+
+			//while( --processCount > 0 ){
+				this.process();
+
+				saveTime = true;
+			//}
+
+			if (saveTime)
+				this.timeOld = timeNow - (timeDelta % this.processInterval);
+
+			this.render();
+
+			if (timeNow - this.timeFrame > timeMgr.StMS) {
+				this.timeFrame = timeNow;
+
+				Notif.sendNotif(Notif.ids.fps, this.frames);
+
+				this.frames = 0;
+			}
+			else
+				this.frames++;
+
+			requestAnimationFrame(() => this.loop());
+		};
+	})()
 }
 
 
 
 module.exports = { DOMComp_Game };
+
+
+
+//#region offlineImports
+const { Notif } = require('@/app/core/notif');
+const { timeMgr } = require('@/app/core/timeMgr');
+
+const { Scene } = require('@/app/modules/scene');
+const { Game } = require('@/app/modules/game/game');
+//#endregion offlineImports
