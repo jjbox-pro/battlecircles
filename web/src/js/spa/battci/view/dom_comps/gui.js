10452aa77095efeda68b7604ccdbc91975495526
denizz <workdenizz@gmail.com>
2022-01-09 13:39:17 +0700

    Added new fps element.

--- web/src/js/spa/battci/view/dom_comps/gui.js
+++ web/src/js/spa/battci/view/dom_comps/gui.js
@@ -12,8 +12,24 @@ class DOMComp_Gui extends DOMComp {
 	calcName() {
 		return 'gui';
 	}
+
+	addNotif(){
+		this.notif.other[Notif.get('fps')] = (fps) => {
+			this.$fps.val(fps);
+		};
+	}
+
+	cacheCont($cont){
+		this.$fps = $cont.find('.gui__fps');
+	}
 }
 
 
 
 module.exports = { DOMComp_Gui };
+
+
+
+//#region offlineImports
+const { Notif } = require('@/app/core/notif');
+//#endregion offlineImports
