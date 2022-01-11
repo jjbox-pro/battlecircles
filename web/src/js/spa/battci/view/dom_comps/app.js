8babde81e6babb9c045b2bc7581ac472dfa5bb35
denizz <workdenizz@gmail.com>
2022-01-11 18:14:21 +0700

    Added notf connection between gui and player's game object.

--- web/src/js/spa/battci/view/dom_comps/app.js
+++ web/src/js/spa/battci/view/dom_comps/app.js
@@ -9,9 +9,15 @@ DOMComp_App.prototype.calcChildren = function(){
 	this.children.gui = DOMComp_Gui;
 };
 
+DOMComp_App.prototype.afterShow = function(){
+	Notif.sendNotif(Notif.ids.nf_onAppRendered);
+};
+
 
 
 //#region offlineImports
+const { Notif } = require('@/app/core/notif');
+
 const { DOMComp_Game } = require('@/spa/battci/view/dom_comps/game');
 const { DOMComp_Gui } = require('@/spa/battci/view/dom_comps/gui');
 //#endregion offlineImports
--- web/src/js/spa/battci/view/dom_comps/gui.js
+++ web/src/js/spa/battci/view/dom_comps/gui.js
@@ -14,13 +14,26 @@ class DOMComp_Gui extends DOMComp {
 	}
 
 	addNotif(){
-		this.notif.other[Notif.get('fps')] = (fps) => {
-			this.$fps.val(fps);
+		this.notif.other[Notif.get('nf_fps')] = (fps) => {
+			this.$gui__fps.val(fps);
+		};
+
+		this.notif.other[Notif.get('nf_ammo')] = (info) => {
+			const $el = this[`$ammo__${info.type}_count`];
+
+			$el.count = $el.count||[];
+
+			$el.count[info.index||0] = info.count;
+
+			this[`$ammo__${info.type}_count`].text($el.count.join('/'));
 		};
 	}
 
 	cacheCont($cont){
-		this.$fps = $cont.find('.gui__fps');
+		this.$gui__fps = $cont.find('.gui__fps');
+
+		this.$ammo__person_count = $cont.find('.ammo__person_count');
+		this.$ammo__weapon_count = $cont.find('.ammo__weapon_count');
 	}
 }
 
